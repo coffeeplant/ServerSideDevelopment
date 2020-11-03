c1a8bf3bc9d6c63632172becce7d4d728af273e5
EC2 Default User <ec2-user@ip-172-31-30-107.ec2.internal>
2020-11-03 22:23:45 +0000

    Functioning cart

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,47 @@
+class CartController < ApplicationController
+  
+  
+  
+  def index
+    
+    # build logic to pass the cart data to the view
+    
+    if session[:cart] then #if sessions/cart exists then
+      
+      # if does exist then all the items to be displayed on the cart index page are all the items in that cart
+      @cart = session[:cart] # @cart is an instance variable
+      
+    else
+      #if the cart doesn't exist then set the cart to be an empty object
+      @cart = {}  #this is a JSON object
+    end
+  end
+  
+  
+  def add
+    # find the id of the product to be added to the cart when someone presses the button
+    id = params[:id] #button will give the id in param, find the product
+    
+    #Find the cart to put the product in or make a new cart
+    if session[:cart] then # if cart exists just use it
+      cart = session[:cart] #make the cart then be an empty cart
+    else
+      
+      session[:cart] = {} #if the cart does not exist at time of button pushing then make one
+    cart = session[:cart]
+    end
+    
+    #control the products in the cart
+    
+    if cart[id] then # see is the product in the cart
+      cart[id] = cart[id] + 1 # if it is then increase quantity by one 
+    else 
+      cart[id] = 1 #if it is not in the cart then add one now
+    end
+    # send the user somewhere now that the item is added
+    
+    redirect_to :action => :index 
+    
+  end
+  
+end
